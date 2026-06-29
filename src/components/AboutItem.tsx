1b600caa19d23f0f9ba58716d369af61a4455136
Evgeny Zuchkov <jenyazuchkov@gmail.com>
2026-06-29 20:58:04 +0300

    add styles for about

--- src/components/AboutItem.tsx
+++ src/components/AboutItem.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import {Icon} from "./icon/Icon.tsx";
+import {FlexWrapper} from "./FlexWrapper.tsx";
 
 type AboutItemPropsType = {
     title: string
@@ -12,21 +13,39 @@ type AboutItemPropsType = {
 export const AboutItem = (props: AboutItemPropsType) => {
     return (
         <StyledAboutItem>
+
             <FirstBlock>
                 <Title>{props.title}</Title>
                 <WorkTime src={props.src} width={'84px'} height={'24px'}/>
             </FirstBlock>
 
-            <SecondBlock>
-                <Icon iconId="icon-grid" width="12px" height="12px"/>
-                <Text>{props.company}</Text>
+            <FlexWrapper justifyContent={"space-between"} alignItems={"center"}>
+                <FlexWrapper flexDirection={'row'} justifyContent={'space-between'} alignItems={"center"} gap={'5px'}>
+                    <Icon iconId="icon-grid" width="12px" height="12px"/>
+                    <Text>{props.company}</Text>
+                </FlexWrapper>
 
-                <Icon iconId="icon-eye-small" width="12px" height="12px"/>
-                <Text>{props.location}</Text>
+                <FlexWrapper gap={'200px'}>
+                    {props.location && (
+                        <FlexWrapper flexDirection={'row'} justifyContent={'space-between'} alignItems={"center"}
+                                     gap={'5px'}>
+                            <Icon iconId="icon-eye-small" width="12px" height="12px"/>
+                            <Text>{props.location}</Text>
+                        </FlexWrapper>
+                    )}
 
-                <Icon iconId="icon-table" width="16px" height="12px"/>
-                <Text>{props.periodOfWork}</Text>
-            </SecondBlock>
+                    <FlexWrapper gap={'5px'} alignItems={"center"}>
+                        <FlexWrapper flexDirection={'row'} justifyContent={'space-between'} alignItems={"center"}>
+                            <Icon iconId="icon-table" width="16px" height="12px"/>
+                        </FlexWrapper>
+
+                        <Text>{props.periodOfWork}</Text>
+                    </FlexWrapper>
+                </FlexWrapper>
+
+
+            </FlexWrapper>
+
         </StyledAboutItem>
     )
 }
@@ -43,11 +62,13 @@ const WorkTime = styled.img`
 const FirstBlock = styled.div`
     display: flex;
     justify-content: space-between;
+    align-items: center;
+    margin-bottom: 4px;
 `
 
-const SecondBlock = styled.div`
-    display: flex;
-    justify-content: space-between;
+const Text = styled.p`
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 2.33333;
+    letter-spacing: 0.08em;
 `
-
-const Text = styled.p``
--- src/layout/sections/about/About.tsx
+++ src/layout/sections/about/About.tsx
@@ -1,57 +1,88 @@
 import styled from "styled-components";
-import {SectionTittle} from "../../../components/SectionTittle.tsx";
-import {Description} from "../../../components/Description.tsx";
 import {AboutItem} from "../../../components/AboutItem.tsx";
 import fullTime from '../../../assets/images/full-time.png'
 import intership from '../../../assets/images/intership.png'
+import {Container} from "../../../components/Container.ts";
+import {Theme} from "../../../styles/Theme.ts";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 
 export const About = () => {
     return (
         <StyledAbout>
+            <Container>
+                <StyledAboutWrap>
+                    <StyledAboutPart>
+                        <StyledSectionTittle>About Me</StyledSectionTittle>
+                        <StyledDescription>The Generator App is an online tool that helps you to export ready-made
+                            templates ready
+                            to
+                            work as your
+                            future website. It helps you to combine slides, panels and other components and export it as
+                            a
+                            set
+                            of
+                            static files: HTML/CSS/JS.</StyledDescription>
+                    </StyledAboutPart>
 
-            <StyledAboutPart>
-                <SectionTittle>About Me</SectionTittle>
-                <Description>The Generator App is an online tool that helps you to export ready-made templates ready to
-                    work as your
-                    future website. It helps you to combine slides, panels and other components and export it as a set
-                    of
-                    static files: HTML/CSS/JS.</Description>
-            </StyledAboutPart>
+                    <StyledAboutPart>
+                        <FlexWrapper flexDirection="column" gap={'30px'}>
+                            <StyledSectionTittle>Work Experience</StyledSectionTittle>
+                            <AboutItem title={'Junior Web Developer'}
+                                       src={fullTime}
+                                       company={'Dr. Rajkumar’s Learning App'}
+                                       location={'Bengaluru'}
+                                       periodOfWork={'Sep 2021 - Dec 2021'}/>
+                            <AboutItem title={'Web Development Intern'}
+                                       src={intership}
+                                       company={'IonPixelz Web Solutions'}
+                                       location={'Bengaluru'}
+                                       periodOfWork={'Sep 2021 - Dec 2021'}/>
+                            <AboutItem title={'SEO / SEM Specialist'}
+                                       src={intership}
+                                       company={'HAAPS'}
+                                       location={'Bengaluru'}
+                                       periodOfWork={'Sep 2021 - Dec 2021'}/>
+                        </FlexWrapper>
 
-            <StyledAboutPart>
-                <SectionTittle>Work Experience</SectionTittle>
-                <AboutItem title={'Junior Web Developer'}
-                           src={fullTime}
-                           company={'Dr. Rajkumar’s Learning App'}
-                           location={'Bengaluru'}
-                           periodOfWork={'Sep 2021 - Dec 2021'}/>
-                <AboutItem title={'Web Development Intern'}
-                           src={intership}
-                           company={'IonPixelz Web Solutions'}
-                           location={'Bengaluru'}
-                           periodOfWork={'Sep 2021 - Dec 2021'}/>
-                <AboutItem title={'SEO / SEM Specialist'}
-                           src={intership}
-                           company={'HAAPS'}
-                           location={'Bengaluru'}
-                           periodOfWork={'Sep 2021 - Dec 2021'}/>
-            </StyledAboutPart>
+                    </StyledAboutPart>
 
-            <StyledAboutPart>
-                <SectionTittle>Education</SectionTittle>
-                <AboutItem title={'Bachelor in Electronics & Communication'}
-                           src={fullTime}
-                           company={'Bangalore Instutute of Technology'}
-                           periodOfWork={'Aug 2015 - Dec 2020'}/>
-            </StyledAboutPart>
+                    <StyledAboutPart>
+                        <StyledSectionTittle>Education</StyledSectionTittle>
+                        <AboutItem title={'Bachelor in Electronics & Communication'}
+                                   src={fullTime}
+                                   company={'Bangalore Instutute of Technology'}
+                                   periodOfWork={'Aug 2015 - Dec 2020'}/>
+                    </StyledAboutPart>
+                </StyledAboutWrap>
+
+            </Container>
+
         </StyledAbout>
 
     )
 }
 
 const StyledAbout = styled.section`
+`
+
+const StyledAboutWrap = styled.div`
     max-width: 710px;
     width: 100%;
+`
+
+const StyledSectionTittle = styled.h2`
+    font-weight: 700;
+    font-size: 42px;
+    line-height: 1.2381;
+    letter-spacing: -0.01em;
+    color: ${Theme.colors.secondaryText};
+    margin: 38px 0;
+`
+
+const StyledDescription = styled.p`
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 1.44444;
 `
 
 const StyledAboutPart = styled.div`
