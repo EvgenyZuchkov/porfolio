43467614fa06d8bfbb831b649f4b4f57040f4f61
Evgeny Zuchkov <jenyazuchkov@gmail.com>
2026-07-03 18:40:57 +0300

    add babel plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // base: '/porfolio/',
+  plugins: [
+    react(),
+
+    babel({
+      plugins: [
+        [
+          'babel-plugin-styled-components',
+          {
+            displayName: true,
+            fileName: true,
+            ssr: true,
+          },
+        ],
+      ],
+    }),
+  ],
+
+  // base: '/portfolio/',
 })
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-//
-//
-// export default defineConfig({
-//   plugins: [
-//     react({
-//       babel: {
-//         plugins: ['babel-plugin-styled-components'],
-//       },
-//     }),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': '/src',
-//     },
-//   },
-// });
-
